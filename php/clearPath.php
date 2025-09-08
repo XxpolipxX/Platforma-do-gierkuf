5d5b17fd4e9434d02ef77acf3926ebd9101ce4eb
XxpolipxX <adam.gabrys@poczta.onet.pl>
2025-09-08 22:24:35 +0200

    Poprawiona ochrona plików
    Jak ktoś będzie próbować wejść na pliki to go wywali na yt na never gonna give you up. Pliki index.php są w folderach żeby jak ktoś se ręcznie wpisze w przeglądarkę to też go tam wywaliło

--- php/clearPath.php
+++ php/clearPath.php
@@ -1,6 +1,6 @@
 <?php
     if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
-        header('Location: ../index.php');
+        header('Location: https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1');
         exit;
     }
     function clearURL() {
