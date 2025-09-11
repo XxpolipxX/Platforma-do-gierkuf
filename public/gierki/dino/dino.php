2140619747f5b09b227147335be4378283cc2a48
JCTheCat <197243635+JCTheCat@users.noreply.github.com>
2025-09-11 19:14:08 +0200

    Nowy CSS

--- public/gierki/dino/dino.php
+++ public/gierki/dino/dino.php
@@ -13,6 +13,18 @@
     <script type="module" src="./main.js" defer></script>
 </head>
 <body>
+    <div class="instrukcje">
+        <span id="Tab">STEROWANIE</span><br>
+        <p>
+            PORUSZANIE SIĘ <br><br>
+            Spacja LUB strzałka w górę - skok
+        </p>
+        <span id="Tab">DŹWIĘKI</span>
+        <p>
+            Gra posiada dźwięki sfx jak i również muzykę. <br>
+        </p>
+    </div>
+
     <div class="header">
         <nav class="nawigacja">
             <a href="../../../menu/index.html">Home</a>
@@ -37,6 +49,14 @@
         <p class="final-time">Czas: 0</p>
         <p class="more-final-score">Finalny wynik: 0</p>
         <button class="restart-game">Zagraj ponownie</button>
+    </div>
+    <div class="Opis">
+        <span id="Tab">ZASADY GRY</span><br>
+        <p>
+            Gracz ma za zadanie jak najdłużej przeżyć, unikając kaktusów skokiem. <br><br>
+            Skok nad każdym kaktusem nagradza gracza +1pkt. <br><br>
+            Kaktusy mogą pojawiać się solo, w parze jak i również w trójkę obok siebie.
+        </p>
     </div>
 
     <?php script() ?>
