52bf7e2bbb64dbb1de5264eed3c3d6ae715d38de
XxpolipxX <132904175+XxpolipxX@users.noreply.github.com>
2025-09-18 13:54:27 +0200

    niepoczebne

--- src/public/wybor-gry.php
+++ src/public/wybor-gry.php
@@ -15,7 +15,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="../logowanie/wyloguj.js"></script>
 </head>
 <body>
     <div id="test"></div>
