45bd139c32010e089f4a3a242947e8fcedcfff63
XxpolipxX <132904175+XxpolipxX@users.noreply.github.com>
2025-09-18 11:57:58 +0200

    zmiana hosta z localhost na db

--- src/php/db.php
+++ src/php/db.php
@@ -3,7 +3,7 @@
         header('Location: https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1');
         exit;
     }
-    $host = 'localhost';
+    $host = 'db';
     $db = 'gierkowo';
     $user = 'root';
     $pass = 'root';
